521ce65ccebf1b4465e1d3239a5b99ae4aaf7c74
Chris Cheung <chris.kt.cheung@gmail.com>
2017-12-23 22:07:48 +0800

    Updated conf.py with mocks

--- docs/conf.py
+++ docs/conf.py
@@ -168,4 +168,13 @@ texinfo_documents = [
 ]
 
 
+# 20171223-220352 Added so that Read the Docs will load C libraries
+from mock import Mock as MagicMock
 
+class Mock(MagicMock):  
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['numpy', 'pandas']  
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
