b51c25c88d15f44882611d6903d7759543e2e1a0
Chris Cheung <chris.kt.cheung@gmail.com>
2017-12-23 22:23:21 +0800

    Updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -177,5 +177,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'pandas']  
+MOCK_MODULES = ['numpy', 'pandas', 'IPython.core']  
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
